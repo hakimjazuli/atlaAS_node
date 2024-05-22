479db4a59a89c3b1e7969a1e70cb3ac0e378b7ff
hakim_jazuli <muhammadhakimjazuli@gmail.com>
2024-05-22 11:15:33 +0700

    ok

--- src/router/_Routes.mjs
+++ src/router/_Routes.mjs
@@ -9,15 +9,4 @@ export class _Routes {
 	 * @type {boolean}
 	 */
 	is_real_route = true;
-	/**
-	 * @param {boolean} [return_as_array=false]
-	 * @returns {string|array}
-	 */
-	static route = (return_as_array = false) => {
-		// const route_array = _FunctionHelpers.class_name_as_array(this.name, []);
-		// if (return_as_array) {
-		// 	return route_array;
-		// }
-		// return `/${path.join(...route_array)}`;
-	};
 }
